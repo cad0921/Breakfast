2ab1598523517b4cc052aed2e671c7dadb33e127
cad0921 <63990831+cad0921@users.noreply.github.com>
2025-11-11 11:58:43 +0800

    Add files via upload

---
+++ Hubs/OrdersHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace BreakFastShop.Hubs
+{
+    public class OrdersHub : Hub
+    {
+        public void CreateOrder(object dto)
+        {
+            Clients.All.orderChanged(new { type = "created", dto, ts = DateTimeOffset.UtcNow });
+        }
+    }
+}
